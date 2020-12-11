5d68bee3bd5f3cb2c55cfaf068132920e59d4f2c
rahulmalreddy <46987739+rahulmalreddy@users.noreply.github.com>
2020-12-11 10:21:26 +0530

    Add files via upload

---
+++ src/main/FactoryProvider.java
@@ -0,0 +1,28 @@
+package com.Helper;
+
+
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+
+public class FactoryProvider {
+	
+	public static SessionFactory factory;
+	
+	@SuppressWarnings("deprecation")
+	public static SessionFactory getFactory() {
+		if(factory == null)
+		{
+			factory =new Configuration().configure("hibernate.cfg.xml").buildSessionFactory();
+			
+		}
+		return factory;
+	}
+//	public void closeFactory() {
+//		if(((Object) factory).isOpen()) 
+//		{
+//			factory.close();
+//		}
+//	}
+
+}
